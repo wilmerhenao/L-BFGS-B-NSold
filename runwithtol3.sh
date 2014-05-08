15213eadca5f0c4dac50cad0c766492764b97c03
wilmer henao <wilmer.henao@caa.columbia.edu>
2014-05-08 11:19:07 -0400

    Code for the thesis

---
+++ runwithtol3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+for ptol in 1d-3
+do
+    for p in 2 1 1.5 1.1 1.01 1.001 1.0001 1.00001 0.99 0.9
+    do
+	for n in 6 8 10 20 50 100 200 1000
+	do
+	    for m in 5 10 20
+	    do
+		echo $ptol $p $n $m
+		./rosenbrockp $m $n $p $ptol >> OUTPUTS/res1d3.txt
+	    done
+	done
+    done
+done
+
+exit 0;
+
